ff26a63f68b0b93fc1bb50fc1bc084bd0cd30427
Oleg Khovayko <khovayko@gmail.com>
2014-05-30 16:41:38 -0400

    do not splot POS TX, if size < POW reward

--- src/wallet.cpp
+++ src/wallet.cpp
@@ -1240,7 +1240,8 @@ bool CWallet::CreateCoinStake(const CKeyStore& keystore, unsigned int nBits, int
     // The following split & combine thresholds are important to security
     // Should not be adjusted if you don't understand the consequences
     static unsigned int nStakeSplitAge = (60 * 60 * 24 * 90);
-    int64 nCombineThreshold = GetProofOfWorkReward(GetLastBlockIndex(pindexBest, false)->nBits) / 3;
+    int64 nPoWReward = GetProofOfWorkReward(GetLastBlockIndex(pindexBest, false)->nBits);
+    int64 nCombineThreshold = nPoWReward / 3;
 
     CBigNum bnTargetPerCoinDay;
     bnTargetPerCoinDay.SetCompact(nBits);
@@ -1333,19 +1334,21 @@ bool CWallet::CreateCoinStake(const CKeyStore& keystore, unsigned int nBits, int
                 nCredit += pcoin.first->vout[pcoin.second].nValue;
                 vwtxPrev.push_back(pcoin.first);
                 txNew.vout.push_back(CTxOut(0, scriptPubKeyOut));
-                if (block.GetBlockTime() + nStakeSplitAge > txNew.nTime)
-                    txNew.vout.push_back(CTxOut(0, scriptPubKeyOut)); //split stake
+                if (block.GetBlockTime() + nStakeSplitAge > txNew.nTime && nCredit > nPoWReward)
+                    txNew.vout.push_back(CTxOut(0, scriptPubKeyOut)); //split stake if (age < 90 && value > POW)
                 if (fDebug && GetBoolArg("-printcoinstake"))
                     printf("CreateCoinStake : added kernel type=%d\n", whichType);
                 fKernelFound = true;
                 break;
-            }
-        }
+            } // if(CheckStakeKernelHash)
+        } // for n
         if (fKernelFound || fShutdown)
             break; // if kernel is found stop searching
-    }
+    } // BOOST_FOREACH(PAIRTYPE(const CWalletTx*, unsigned int))
+
     if (nCredit == 0 || nCredit > nBalance - nReserveBalance)
         return false;
+
     BOOST_FOREACH(PAIRTYPE(const CWalletTx*, unsigned int) pcoin, setCoins)
     {
         // Attempt to add more inputs
