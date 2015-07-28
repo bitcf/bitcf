73584f4019ce496682dc54f7a518eb9f35feba3d
maxihatop <khovayko@gmail.com>
2015-07-28 15:14:59 -0400

    Added 3 min rest after successful PoS - add randomization

--- src/main.cpp
+++ src/main.cpp
@@ -4107,7 +4107,8 @@ void BitcoinMiner(CWallet *pwallet, bool fProofOfStake)
                 SetThreadPriority(THREAD_PRIORITY_NORMAL);
                 CheckWork(pblock.get(), *pwalletMain, reservekey);
                 SetThreadPriority(THREAD_PRIORITY_LOWEST);
-		Sleep(3 * 60 * 1000); // Rest for 3 minutes after successful block to preserve close quick
+		// Rest for ~3 minutes after successful block to preserve close quick
+		Sleep(60 * 1000 + GetRand(4 * 60 * 1000));
             }
             Sleep(pos_timio);
             continue;
