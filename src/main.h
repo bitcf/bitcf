c3292c9dc8c177a25ce8ea2dbaa26c2da059449b
EvgenijM86 <Evgenij1@hotmail.com>
2015-12-01 11:57:32 +0200

    revert index change, add more blocks to entropy bit table

--- src/main.h
+++ src/main.h
@@ -1373,11 +1373,8 @@ public:
         nFlags |= BLOCK_PROOF_OF_STAKE;
     }
 
-    unsigned int GetStakeEntropyBit(int32_t height) const
+    unsigned int GetStakeEntropyBit() const
     {
-        if (height > -1 && height <= vEntropyBits_number_of_blocks)
-            return (vEntropyBits[height >> 5] >> (height & 0x1f)) & 1;
-
         return ((nFlags & BLOCK_STAKE_ENTROPY) >> 1);
     }
 
@@ -1406,7 +1403,7 @@ public:
         return strprintf("CBlockIndex(nprev=%08x, pnext=%08x, nFile=%d, nBlockPos=%-6d nHeight=%d, nMint=%s, nMoneySupply=%s, nFlags=(%s)(%d)(%s), nStakeModifier=%016"PRI64x", nStakeModifierChecksum=%08x, hashProofOfStake=%s, prevoutStake=(%s), nStakeTime=%d merkle=%s, hashBlock=%s)",
             pprev, pnext, nFile, nBlockPos, nHeight,
             FormatMoney(nMint).c_str(), FormatMoney(nMoneySupply).c_str(),
-            GeneratedStakeModifier() ? "MOD" : "-", GetStakeEntropyBit(nHeight), IsProofOfStake()? "PoS" : "PoW",
+            GeneratedStakeModifier() ? "MOD" : "-", GetStakeEntropyBit(), IsProofOfStake()? "PoS" : "PoW",
             nStakeModifier, nStakeModifierChecksum, 
             hashProofOfStake.ToString().c_str(),
             prevoutStake.ToString().c_str(), nStakeTime,
